9ef51867393bc1ca80de88db63e9d984d8fb78b2
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-15 01:20:57 -0300

    function

--- site.php
+++ site.php
@@ -11,12 +11,28 @@
      $idade = 16;
      $cidade = "Santa Cruz do Sul";
      $pais = "Brasil";
-     echo "<h1>Teste \"aspas\"</h1>"
+     define ("curso","Técnico em TI");
+     define ("duracao",1200);
+     echo "<h1>Teste \"aspas\"</h1>";
+     echo "O meu curso é: " . curso;
+     echo "<br>";
+     echo "A duraçao do curso é de: " . duracao . "horas"; 
+    $numero = 0;
+     function adicionar_numero () 
+     {
+        global $numero;
+        $numero += 5; 
+        echo "<br>";
+        echo "Valor do número: " . $numero; 
+
+     }
+     echo "valor do número inicial: " . $numero;
+     adicionar_numero()
+     
     ?>
     <h1>Meu nome é: "<?php echo  $nome; ?>"</h1>
     <h2>Minha idade é: <?php echo $idade; ?></h2>
     <h3>Moro na cidade: <?php echo $cidade; ?> 
     <br>E no país: <?php echo $pais; ?></h3>
-
 </body>
 </html>
