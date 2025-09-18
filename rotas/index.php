77545505324db9312e4d3aa8c530426549c4b11d
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-17 21:56:28 -0300

    rotas funcionando

--- rotas/index.php
+++ rotas/index.php
@@ -1,7 +1,9 @@
 <?php
 
 $rota = $_SERVER;
+$endpoint = $_SERVER["REQUEST_METHOD"] . "" . $_SERVER["REQUEST_URI"];
 echo "rotasPHP";
 echo "<pre>";
-print_r($rota);
-echo "<pre>";
+print_r($rota); 
+print_r($endpoint);
+echo "<pre>";  
