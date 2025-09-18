7ba3f674e42808ed8ec75a0e20ddd560b101392c
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-17 22:00:13 -0300

    rotas verificações

--- rotas/index.php
+++ rotas/index.php
@@ -2,8 +2,16 @@
 
 $rota = $_SERVER;
 $endpoint = $_SERVER["REQUEST_METHOD"] . "" . $_SERVER["REQUEST_URI"];
+
+if(!strpos($_SERVER["REQUEST_URI"], "caixa")) {
+    echo "Rota inválida";
+    return;
+};
 echo "rotasPHP";
 echo "<pre>";
 print_r($rota); 
 print_r($endpoint);
 echo "<pre>";  
+
+
+// rotas/index.php/talrota
