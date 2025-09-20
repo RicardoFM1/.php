0934b21733e696ba32ae00e5b11af7e2b66a5b71
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-19 22:03:58 -0300

    infos do back no front

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let counter = 0
 const clickMe = document.getElementById("clickMe")
 const counterDoc = document.getElementById("counter")
 const body = document.getElementById("body")
+const infosRes = document.getElementById("info-back")
 
 clickMe.addEventListener("click", () => {
     counter++
@@ -13,8 +14,14 @@ clickMe.addEventListener("click", () => {
 })
 
 const pegarDados = async() => {
-    const res = await fetch("http://localhost:3001/usuarios")
-    console.log(res.data)
+    const res = await fetch("http://localhost:3001/usuarios",
+     {Headers: {'Accept': 'application/json'}}
+    )
+    const info = await res.json()
+    console.log(info)
+    const infostring = info.email + " " + info.senha
+   infosRes.innerText = infostring
+
 }
 document.addEventListener("DOMContentLoaded", () => {
 
