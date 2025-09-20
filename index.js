91117367ca8b1c8b09694ac3c696bd36768490f9
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-19 21:19:25 -0300

    get e post(com um echo apenas)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let counter = 0
 
+
 const clickMe = document.getElementById("clickMe")
 const counterDoc = document.getElementById("counter")
 const body = document.getElementById("body")
@@ -8,11 +9,19 @@ const body = document.getElementById("body")
 clickMe.addEventListener("click", () => {
     counter++
     counterDoc.innerText = "Clicks: " + counter
+
 })
 
+const pegarDados = async() => {
+    const res = await fetch("http://localhost:3001/usuarios")
+    console.log(res.data)
+}
 document.addEventListener("DOMContentLoaded", () => {
 
     const p = document.createElement("p")
     p.innerText = "pAdicionado"
     body.appendChild(p)
+    pegarDados()
 })
+
+
