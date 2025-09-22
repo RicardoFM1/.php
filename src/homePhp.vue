aa51fb4ae8b019fe8b7a668a98c9a6639edae608
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-21 21:32:33 -0300

    get do banco no front

--- src/homePhp.vue
+++ src/homePhp.vue
@@ -4,24 +4,28 @@
         <h1>Visualizar dados:</h1>
     </div>
     <div class="divGet">
-        <h1>Puxar informações dos usuarios no sistema:</h1>
-        <button >Pegar informações</button>
+        <h1 v-if="amostra">Puxar informações dos usuarios no sistema:</h1>
+        <button @click="pegarDados">Pegar informações</button>
+        <p></p>
         
     </div>
 </template>
 
-<script>
-export default {
-    name: "HomePHP"
-}
-const pegarDados = async() => {
-    const res = await fetch("http://localhost:8080/usuarios",
-     {Headers: {'Accept': 'application/json'}}
-    )
-    const info = await res.json()
-    console.log(info)
-    const infostring = info.email + " " + info.senha
-   infosRes.innerText = infostring
 
-}
-</script>
+    <script setup>
+    import { ref } from 'vue'
+
+    const amostra = ref(true)
+    const idade = ref("22")
+    console.log(idade.value)
+
+   
+    const pegarDados = async () => {
+        const res = await fetch("http://localhost:8080/usuarios",
+         {headers: {'Accept': 'application/json'}}
+        )
+        const info = await res.json()
+        console.log(info, "infoBackend")
+    }
+    
+    </script>
